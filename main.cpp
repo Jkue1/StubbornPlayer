3c1d6d1ff54874d07b38263ea040889a006c6809
jkue1 <jkue1@mail.csuchico.edu>
2018-02-28 19:49:46 -0800

    finished

--- main.cpp
+++ main.cpp
@@ -6,6 +6,54 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int number;
+  int i;
+  int win = 0;
+  char letter;
+  game.newGame(); 
+  cout<<"How many times do you want to play the game?\n";
+  cin>>number;
+
+  
+  for ( i = 0; i < number; i++)
+  {
+   
+    cout<<"Which door do you choose? (A/B/C)\n";
+    cin>>letter;
+    if (letter == 'a' || letter == 'A')
+    {
+        game.guessDoorC();
+    }
+    if (letter == 'b' || letter == 'B')
+    {
+        game.guessDoorC();
+    }
+    else 
+    {
+        game.guessDoorC();
+    }
+    cout<<"Pick again for your second choice\n";
+    cin>>letter;
+    if (letter == 'a' || letter == 'A')
+    {
+        game.guessDoorC();
+    }
+    if (letter == 'b' || letter == 'B')
+    {
+        game.guessDoorC();
+    }
+    else {game.guessDoorC();}
+
+    if (game.isWinner() == true)
+    {
+        cout<<win++<<"You won!\n"; 
+    }
+    else 
+    {
+        cout<<"You lose :(\n"; 
+    }
+   
+  }  
+cout<<" You won: "<<win<<endl;
   return 0;
 }
